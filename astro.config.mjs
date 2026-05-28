cd5920ae6e600b49f4fa1323efe890098b9a4d3d
Justin Farris <justin@farrisfam.org>
2026-05-27 21:29:07 -0600

    cleaning up for production

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-// astro/config
 export default defineConfig({
   site: 'https://justin.farrisfam.org',
-  output: 'static', // explicitly static (this is the default, but good to be clear)
+  output: 'static',
   integrations: [mdx(), sitemap()],
 });
